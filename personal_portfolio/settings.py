6431a31402abda4c97659b85b382920f1351f1c1
ara7788 <test3007@ya.ru>
2023-07-31 09:00:53 +0300

    DEBUG=False

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)84=-k-eo(0+_&y^u#)9vgr)@m8!okd2tb53ivj8i!)vtgf&42'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['testtestovich728.pythonanywhere.com']
 
